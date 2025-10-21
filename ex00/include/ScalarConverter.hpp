8deacd4dc969387c89e84d0a04b958839afec0b8
kopalych <kopalychmail@gmail.com>
2025-10-21 08:38:11 +0200

    ex00: missing include fix, and non-instantiable class

--- ex00/include/ScalarConverter.hpp
+++ ex00/include/ScalarConverter.hpp
@@ -6,9 +6,11 @@
 /*   By: akostian <akostian@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 16:41:14 by akostian          #+#    #+#             */
-/*   Updated: 2025/08/20 21:43:53 by akostian         ###   ########.fr       */
+/*   Updated: 2025/10/21 08:34:44 by akostian         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include <stdlib.h>
 
 #include <climits>
 #include <iostream>
@@ -17,13 +19,15 @@
 #include "colors.hpp"
 
 class ScalarConverter {
-   public:
+   private:
 	ScalarConverter(){};
 	ScalarConverter(const ScalarConverter& other) { (void)other; };
 	ScalarConverter& operator=(const ScalarConverter& other) {
 		(void)other;
 		return *this;
 	};
+
+   public:
 	virtual ~ScalarConverter() = 0;
 
 	static void convert(std::string input);
